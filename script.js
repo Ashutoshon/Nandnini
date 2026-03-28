7a36d784c819fb32b231b54019a3db9bc52af51a
Ashutoshon <42493086+Ashutoshon@users.noreply.github.com>
2026-03-28 19:06:09 +0530

    Updating script.js with complete JavaScript functions

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-function nextScreen(num) {
-    if (num === 5) {
-        pauseMusic();
-    } else if (num === 6) {
-        resumeMusic();
-    }
-    // Additional logic for handling other screens can go here
+function checkPassword(password) {
+    const storedPassword = 'yourPassword';
+    return password === storedPassword;
+}
+
+function nextScreen() {
+    console.log('Navigating to the next screen');
+}
+
+function revealMessage(message) {
+    console.log(message);
+}
+
+function updateCounter(counterId) {
+    const counter = document.getElementById(counterId);
+    counter.innerText = parseInt(counter.innerText) + 1;
+}
+
+function typeStory(story) {
+    console.log('Typing story:', story);
+}
+
+function loadChat() {
+    console.log('Chat loading...');
+}
+
+function playVoice(voice) {
+    console.log('Playing voice:', voice);
+}
+
+function loveResponse() {
+    console.log('Love response triggered');
+}
+
+function musicControls(action) {
+    console.log(`Music control action: ${action}`);
+}
+
+function createConfetti() {
+    console.log('Creating confetti');
+}
+
+function drawStars() {
+    console.log('Drawing stars');
 }
